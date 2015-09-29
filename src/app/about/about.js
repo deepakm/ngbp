afecc45d5e7df8dce485f1edb13cb9c95c5ae793
Deepak Manoharan <deepakm@gmail.com>
2015-09-29 01:32:01 -0700

    Support for displaying data

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -17,13 +17,37 @@ angular.module( 'ngBoilerplate.about', [
   });
 })
 
-.controller( 'AboutCtrl', function AboutCtrl( $scope ) {
+.filter('prettyJSON', function () {
+    function syntaxHighlight(json) {
+      return JSON ? JSON.stringify(json, null, '  ') : 'your browser doesnt support JSON so cant pretty print';
+    }
+    return syntaxHighlight;
+})
+
+.controller( 'AboutCtrl', function AboutCtrl( $scope, $http ) {
   // This is simple a demo for UI Boostrap.
   $scope.dropdownDemoItems = [
     "The first choice!",
     "And another choice for you.",
     "but wait! A third!"
   ];
+  $scope.query = "";
+  $scope.data = "";
+
+  $scope.queryChanged = function() {
+      var r = $scope.query.split(' ').join('+');
+      var url = "http://localhost:9002/store/query/?query=" + r;
+      $http.get(url).then(function(response) {
+          $scope.data = response.data;
+      });
+  };
+
+  $scope.notSorted = function(obj){
+        if (!obj) {
+            return [];
+        }
+        return Object.keys(obj);
+    };
 })
 
 ;
